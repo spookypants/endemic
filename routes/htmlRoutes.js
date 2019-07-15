40e220792500e2db0661c34edb1300fed13a14fe
sylvo <49357334+sjonon@users.noreply.github.com>
2019-07-15 13:48:05 -0500

    deleted extra sql files no longer needed

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
-        msg: "Welcome!",
+        msg: "Welcome! You are now using Passport with Sequelize! Go team!",
         examples: dbExamples
       });
     });
