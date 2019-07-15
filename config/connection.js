4b8570062410c159fb595a9cbfc4bbbb6f00d486
sylvo <49357334+sjonon@users.noreply.github.com>
2019-07-15 11:48:57 -0500

    updated connection.js file to comply with lint rules

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,12 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'endemic_db'
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "endemic_db"
   });
-};
+}
 connection.connect();
 // Export connection for our ORM to use.
 module.exports = connection;
