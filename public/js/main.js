ee623eb0424be3e5ef10babca070d58b5d108379
jebaquera <jebaquera@gmail.com>
2019-07-15 14:07:23 -0500

    processed through lint.

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,4 @@
 $("#sign-up").on("click", function(event) {
-    event.preventDefault();
-    window.location.href = "/signup";
+  event.preventDefault();
+  window.location.href = "/signup";
 });
-  
-  // $("#sign-in").on("click", function(event) {
-  //   event.preventDefault();
-  //   var user = {
-  //     email: $("#email").val(),
-  //     accountKey: $("#account_password").val()
-  //   };
-  //   $.post("/login", user, function(results) {
-  //     if (results) {
-  //       $.get("/api/users/key", user, function(results) {
-  //         if (results) {
-  //           $(location).attr("href", "/ludus-magnus");
-  //         } else {
-  //           $(location).attr("href", "/character");
-  //         }
-  //       });
-  //     } else {
-  //       $("#account-info").modal("close");
-  //       alert("oops something went wrong, please try again!");
-  //     }
-  //   });
-  // });
-  
