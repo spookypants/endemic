8be71ae11711c11febd434473d0d4a74ddc9c821
sylvo <49357334+sjonon@users.noreply.github.com>
2019-07-17 11:15:10 -0500

    api routes created....for the third time...maybe final??

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,40 @@
 var db = require("../models");
+var passport = require("../config/passport");
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get all players
+  app.get("/api/players", function(req, res) {
+    db.Players.findAll({}).then(function(dbPlayers) {
+      res.json(dbPlayers);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // Create a new player
+  app.post("/api/players", function(req, res) {
+    db.Players.create(req.body).then(function(dbPlayers) {
+      res.json(dbPlayers);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  app.delete("/api/mychar", isAuthenticated, function(req, res) {
+    db.Players.destroy({ where: { userName: res.body.userName } }).then(function(dbPlayers) {
+      res.json(dbPlayers);
     });
   });
+  //register 
+  app.post("/api/signup", function(req, res){
+    db.Players.create({
+      userName: req.body.userName,
+      password: req.body.password
+    }).then(function(dbPlayers){
+      res.json(dbPlayers);
+    });
+  });
+
+  app.post("api/login", passport.authenticate("local"), function(req, res){
+    res.json(req.user);
+  });
+
 };
