1312e69fc014a8b06ac6fbeaa28452d4e32d1357
sylvo <49357334+sjonon@users.noreply.github.com>
2019-07-17 22:52:20 -0500

    added id for links in navbar, updated code for landing.js and account.js to reflect new ids

--- public/js/landing.js
+++ public/js/landing.js
@@ -9,19 +9,17 @@ $("#sign-in").on("click", function(event) {
     userName: $("#userName").val(),
     password: $("#account_password").val()
   };
-  $.post("/login", user, function(results) {
+  $.get("/api/login", user, function(results) {
     if (results) {
-      $.get("/api/users/key", user, function(results) {
-        if (results) {
           $(location).attr("href", "/welcomeBack");
         } else {
           $(location).attr("href", "/character");
         }
       });
-    } else {
-      $("#account-info").modal("close");
-      alert("There was an error, please try again.");
-    }
-  });
-});
+//     } else {
+//       $("#account-info").modal("close");
+//       alert("There was an error, please try again.");
+//     }
+//   });
+// });
   
