c4208f59f4ec4b69d1e66e4b53a13b56942bf567
sylvo <49357334+sjonon@users.noreply.github.com>
2019-07-18 11:02:15 -0500

    added link functionality to main page, got form working for creating new account

--- public/js/landing.js
+++ public/js/landing.js
@@ -9,17 +9,19 @@ $("#sign-in").on("click", function(event) {
     userName: $("#userName").val(),
     password: $("#account_password").val()
   };
-  $.get("/api/login", user, function(results) {
+  $.post("/api/login", user, function(results) {
     if (results) {
-          $(location).attr("href", "/welcomeBack");
-        } else {
-          $(location).attr("href", "/character");
-        }
-      });
-//     } else {
-//       $("#account-info").modal("close");
-//       alert("There was an error, please try again.");
-//     }
-//   });
+      $(location).attr("href", "/welcomeBack");
+    } else {
+      $(location).attr("href", "/characters");
+    }});
+  //     } else {
+  //       $("#account-info").modal("close");
+  //       alert("There was an error, please try again.");
+  //     }
+  //   });
+});
+// $("#play").on("click", function(event){
+//   event.preventDefault();
+//   window.location.href = "/characters";
 // });
-  
