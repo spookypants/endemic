0c09e25f05c573d48ef4ea49297f163ad0633c9b
Gabe <42557432+spookypants@users.noreply.github.com>
2019-07-21 14:26:10 -0500

    trouble shooting connection

--- config/connection.js
+++ config/connection.js
@@ -13,6 +13,12 @@ if (process.env.JAWSDB_URL) {
     
   });
 }
-connection.connect();
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connceting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 // Export connection for our ORM to use.
 module.exports = connection;
